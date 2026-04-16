a388c329077848efa6652dc47e7d32260a7c5d76
DuckylikesBatman <maher.aboudiab@hotmail.com>
2026-04-16 23:52:36 +0300

    added new shoes

--- js/main.js
+++ js/main.js
@@ -1,3 +1,19 @@
+function initCurrentNav() {
+  const parts = window.location.pathname.split('/').filter(Boolean);
+  let current = parts[parts.length - 1] || 'index.html';
+  if (!current.includes('.')) current = 'index.html';
+  document.querySelectorAll('.nav__link').forEach((link) => {
+    const href = link.getAttribute('href');
+    if (href === current) {
+      link.setAttribute('aria-current', 'page');
+    } else {
+      link.removeAttribute('aria-current');
+    }
+  });
+}
+
+initCart();
+initCurrentNav();
 initCursor();
 initProductFilters();
 initScrollReveal();
